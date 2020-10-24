b2b770f7ffd64447768dd12f5f6b6dc1a1a72172
Valentin Mara <valentin.mara11@gmail.com>
2020-10-24 01:44:47 -0500

    Added to app.py

--- walk/app.py
+++ walk/app.py
@@ -20,6 +20,40 @@ seed = int(seed_soup.find('div', class_="stats").span.text)
 def index():
     return render_template("index.html")
 
+@app.route('/runescape')
+
+def rs():
+    request = requests.get(f"https://oldschool.runescape.wiki/?search={runescape}&title=Special:Search&limit=250&fulltext=1")
+    soup = BeautifulSoup(request.content, "html.parser")
+    titles = soup.find_all('div')
+
+    title = titles[seed].text
+
+    body_link = titles[seed].get('href')
+    request2 = requests.get(f"https://oldschool.runescape.wiki{body_link}")
+    soup = BeautifulSoup(request2.content, "html.parser")
+
+    body = soup.p.text
+
+    return render_template("herb_patch.html", head_text = title, body_text = body)
+
+@app.route('/herb_run')
+
+def run():
+    request = requests.get(f"https://oldschool.runescape.wiki/?search={herb_run}&title=Special:Search&limit=250&fulltext=1")
+    soup = BeautifulSoup(request.content, "html.parser")
+    titles = soup.find_all('div')
+
+    title = titles[seed].text
+
+    body_link = titles[seed].get('href')
+    request2 = requests.get(f"https://oldschool.runescape.wiki{body_link}")
+    soup = BeautifulSoup(request2.content, "html.parser")
+
+    body = soup.p.text
+
+    return render_template("herb_patch.html", head_text = title, body_text = body)
+
 @app.route('/herb_patch')
 
 def patch():
