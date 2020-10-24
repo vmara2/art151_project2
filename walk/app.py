959f57cf5eabf24143704893bdf4f966cc40a3ff
Valentin Mara <valentin.mara11@gmail.com>
2020-10-24 00:47:27 -0500

    Fixed herb patch v3

--- walk/app.py
+++ walk/app.py
@@ -22,14 +22,14 @@ def index():
 
 @app.route('/herb_patch')
 
-def patch(): 
+def patch():
     request = requests.get(f"https://oldschool.runescape.wiki/?search={herb_patch}&title=Special:Search&limit=250&fulltext=1")
     soup = BeautifulSoup(request.content, "html.parser")
 
     divs = soup.find_all('div')
     titles = soup.find_all('a')
 
-    title = titles[seed].text
-    body = divs[seed].text
+    head_text = titles[seed].text
+    body_text = divs[seed].text
 
-    return render_template("herb_patch.html", title = head_text, body = body_text)
+    return render_template("herb_patch.html", head_text = title, body_text = body)
