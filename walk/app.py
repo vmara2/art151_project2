1525026ae311918e7dd7d882294665d856706420
Valo Mara <linchiloco@LAPTOP-5KPHHSR8.localdomain>
2020-10-26 03:22:46 -0500

    added herb_seed page and updated app.py

--- walk/app.py
+++ walk/app.py
@@ -73,3 +73,21 @@ def patch():
     image = f"https://oldschool.runescape.wiki{soup.img.get('src')}"
 
     return render_template("herb_patch.html", head_text = title, body_text = body, image = image)
+
+@app.route('/herb_seed')
+
+def patch():
+    request = requests.get(f"https://oldschool.runescape.wiki/?search={herb_seed}&title=Special:Search&limit=250&fulltext=1")
+    soup = BeautifulSoup(request.content, "html.parser")
+    titles = soup.find_all('a')
+
+    title = titles[seed].text
+
+    body_link = titles[seed].get('href')
+    request2 = requests.get(f"https://oldschool.runescape.wiki{body_link}")
+    soup = BeautifulSoup(request2.content, "html.parser")
+
+    body = soup.p.text
+    image = f"https://oldschool.runescape.wiki{soup.img.get('src')}"
+
+    return render_template("herb_seed.html", head_text = title, body_text = body, image = image)
